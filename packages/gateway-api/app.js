3811f4f4707a4136eeb622c9e3bede6ab3d5341a
tom-dolidon <123558946+tom-dolidon@users.noreply.github.com>
2023-01-25 14:34:16 +0000

    Add part 9 : Gateway API

--- packages/gateway-api/app.js
+++ packages/gateway-api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const pino = require('pino-http')();
-
+const auth = require('./middlewares/auth');
 const router = require('./routes/index');
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(pino);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(auth);
 app.use('/api', router);
 
 module.exports = app;
